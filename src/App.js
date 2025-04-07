b5e3741b215173863b7cda62def3fa497a630f5b
Ducky0b <vanviet28052002@gmail.com>
2025-04-07 23:00:48 +0700

    add pj

--- src/App.js
+++ src/App.js
@@ -1,37 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
+import Main from "./components/Main";
 import "./App.css";
 function App() {
-  const [count, setCount] = useState(0);
-  let color = count > 0 ? "green" : count < 0 ? "red" : "black";
-  // if (count > 0) {
-  //   color = "green";
-  // } else if (count < 0) {
-  //   color = "red";
-  // } else {
-  //   color = "black";
-  // }
-  const decrease = () => setCount(count - 1);
-  const increase = () => setCount(count + 1);
-  const reset = () => setCount(0);
-  return (
-    <div className="container">
-      <h1>Counter</h1>
-      <span style={{ color }} id="value">
-        {count}
-      </span>
-      <div className="btn-group">
-        <button onClick={decrease} className="btn btn-decrease">
-          DECREASE
-        </button>
-        <button onClick={reset} className="btn btn-reset">
-          RESET
-        </button>
-        <button onClick={increase} className="btn btn-increase">
-          INCREASE
-        </button>
-      </div>
-    </div>
-  );
+  return <Main />;
 }
 
 export default App;
